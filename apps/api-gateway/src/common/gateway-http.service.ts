d3cd3035b8fa1d818510aaa217ea91542b495591
Muhammad Subhan <sbhnamir456@gmail.com>
2026-06-13 17:09:22 +0500

    improvements in api-gatewa

--- apps/api-gateway/src/common/gateway-http.service.ts
+++ apps/api-gateway/src/common/gateway-http.service.ts
@@ -3,41 +3,27 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class GatewayHttpService {
-
-  async get(
-    url: string,
-    authorization?: string,
-    params?: any,
-  ) {
-    const response = await axios.get(
-      url,
-      {
-        headers: {
-          Authorization:
-            authorization,
-        },
-        params,
+  async get(url: string, authorization?: string, params?: any) {
+    console.log(`[Gateway] GET ${url}`);
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: authorization,
       },
-    );
+      params,
+      timeout: 5000,
+    });
 
     return response.data;
   }
 
-  async post(
-    url: string,
-    body: any,
-    authorization?: string,
-  ) {
-    const response = await axios.post(
-      url,
-      body,
-      {
-        headers: {
-          Authorization:
-            authorization,
-        },
+  async post(url: string, body: any, authorization?: string) {
+    console.log(`[Gateway] GET ${url}`);
+    const response = await axios.post(url, body, {
+      headers: {
+        Authorization: authorization,
       },
-    );
+      timeout: 5000,
+    });
 
     return response.data;
   }
